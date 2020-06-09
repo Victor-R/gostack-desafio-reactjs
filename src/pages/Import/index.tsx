d8769a8db4285d393834e9e541190b8e85f40700
Victor-R <victor_ribeiro.pira@hotmail.com>
2020-06-09 18:38:34 -0300

    Updated pages with backend integration and page navigation

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,28 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    uploadedFiles.forEach(file => data.append('file', file.file));
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
+      setUploadedFiles([]);
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const newFiles: FileProps[] = files.map(fileRaw => {
+      return {
+        file: fileRaw,
+        name: fileRaw.name,
+        readableSize: fileRaw.size.toString(),
+      };
+    });
+
+    setUploadedFiles([...uploadedFiles, ...newFiles]);
   }
 
   return (
--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,12 @@
-const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+const formatValue = (value: number | Date): string => {
+  if (value instanceof Date) {
+    return Intl.DateTimeFormat('pt-BR').format(new Date(value));
+  }
+  return Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+    minimumFractionDigits: 2,
+  }).format(value);
+};
 
 export default formatValue;
